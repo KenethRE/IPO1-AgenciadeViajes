0d9d61eb420a2dc7564fa4394a60e78c749c0edd
pieda <pieda@LAPTOP-MVBH5M4M>
2021-11-27 18:43:01 +0100

    Inicio

---
+++ ConfirmacionParcela.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IPO1_AgenciadeViajes
+{
+    /// <summary>
+    /// Lógica de interacción para ConfirmacionParcela.xaml
+    /// </summary>
+    public partial class ConfirmacionParcela : Window
+    {
+        public ConfirmacionParcela()
+        {
+            InitializeComponent();
+        }
+
+        private void AceptarConfirmacionParcela(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(string.Format("El usuario: {0}, Ha reservado la parcela {1}{2}", _nombre.Text,
+            Environment.NewLine, _comentario.Text));
+        }
+    }
+}
