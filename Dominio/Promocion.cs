50ed405be5ad73e1338ba8285ad59008f4f56b6c
pieda <pieda@LAPTOP-MVBH5M4M>
2021-12-29 23:12:26 +0100

    Persistencia, dominio, presentacion

---
+++ Dominio/Promocion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPO1_AgenciadeViajes.Dominio
+{
+    class Promocion
+    {
+        public string Titulo { set; get; }
+        public string Descripcion { set; get; }
+        public Uri Foto { set; get; }
+        public bool Estado { set; get; }
+        public Uri URL_IMDB { set; get; }
+
+        public Promocion(string titulo, string descripcion, Uri foto, bool estado)
+        {
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Foto = foto;
+            Estado = estado;
+        }
+    }
+}
---
+++ Dominio/RutaSenderista.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPO1_AgenciadeViajes.Dominio
+{
+    class RutaSenderista
+    {
+        public string Titulo { set; get; }
+        public string Descripcion { set; get; }
+        public Uri Mapa { set; get; }
+        public string Horario { set; get; }
+        public string Monitor { set; get; }
+        public string Encuentro { set; get; }
+        public string Capacidad { set; get; }
+        public string Dificultad { set; get; }
+        public Uri URL_IMDB { set; get; }
+
+        public RutaSenderista(string titulo, string descripcion, Uri mapa, string horario, string monitor, string encuentro, string capacidad, string dificultad)
+        {
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Mapa = mapa;
+            Horario = horario;
+            Monitor = monitor;
+            Encuentro = encuentro;
+            Capacidad = capacidad;
+            Dificultad = dificultad;
+        }
+    }
+}
